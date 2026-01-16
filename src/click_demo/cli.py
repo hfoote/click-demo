7ed1d60064f2009ea0a8040c422f4f2299513755
hfoote <haydenfoote@arizona.edu>
2026-01-16 16:55:14 -0700

    Add version option

--- src/click_demo/cli.py
+++ src/click_demo/cli.py
@@ -2,15 +2,17 @@ import click
 
 # defines the command group
 @click.group()
+@click.version_option() # adds a --version option to the group
 def cli():
 	pass
 
 @cli.command() # defines a command under the group cli
 # options are defined with the option decorator. First argument is the option, 
 # second argument (optional) is the variable that click will pass the option to
-# in the function. If this idn't specified, click will infer the correct variable by 
+# in the function. If this isn't specified, click will infer the correct variable by 
 # removing leading dashes from the option name. You can set default values and a 
 # message to be displayed in the command's help page. 
+# For more on options, see https://click.palletsprojects.com/en/stable/options/
 @click.option('--count', 'n', default=1, help='Number of greetings')
 @click.option('--name', help="The person to greet")
 def greeting(n, name):
@@ -20,3 +22,5 @@ def greeting(n, name):
 			click.echo(f"Hello, {name}!")
 		else:
 			click.echo(f"Hello!")
+
+
